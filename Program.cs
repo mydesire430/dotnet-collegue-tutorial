3ed366cf680766ecb35c49d29ec5ef6ae5254007
sulaimanadegoke <sulaimanogundare@gmail.com>
2024-04-28 13:25:26 +0100

    myfirstcommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace thebulbroughwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
